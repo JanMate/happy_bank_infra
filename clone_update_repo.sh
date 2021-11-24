69b11114878e1ca7edaa9a6d4682d83e2a462e58
Jan Matejka <jan.matejka142@gmail.com>
2021-11-24 22:22:19 +0100

    minor: Add git exam script
    It adds skeleton of git exam in clone_update_repo.sh.
    
    Closes nothing

---
+++ clone_update_repo.sh
@@ -0,0 +1,79 @@
+# Add announcement that this script has just started with timestamp
+# TODO
+
+# Clone the "happy_bank_core" repo from Github
+# TODO
+
+# Create a new branch "update-readme" and checkout it
+# TODO
+
+# Update README in cloned repo - copy there the differences of the file in this repo
+# Commit message and Workflow sub-sections (Hint: Try to use "head" or "tail" command + pipe "|" to redirect output)
+# TODO
+
+# Add your change to git stage area
+# TODO
+
+# Show user git status
+# TODO
+
+# Commit the new change with message: "minor: Distribute GitOps instructions to README.md"
+# TODO
+
+# Verify you are up-to-date
+# TODO
+
+# Push changes to origin
+# TODO
+
+# Show git log with 5 latest commits
+# TODO
+
+# Print git blame of the updated file
+# TODO
+
+# Create a new tag "0.0.1" of the latest change
+# TODO
+
+# Revert the last change to cancel it
+# TODO
+
+# Reset the graph on the branch to the beginning, but keep all changes in stage area
+# Compare the difference between git revert and reset in your mind
+# TODO
+
+# add new section
+# TODO
+
+# Add your change to git stage area
+# TODO
+
+# Commit the new change with message: "minor: Distribute GitOps instructions to README.md"
+# TODO
+
+# add another section
+# TODO
+
+# Add your change to git stage area
+# TODO
+
+# Commit the new change to previous commit (new change must be part of the existing commit)
+# TODO
+
+# Show difference between new and previous commit on the updated file
+# TODO
+
+# Push changes to origin
+# TODO
+
+# Switch to main branch
+# TODO
+
+# Remove "update-readme" branch on origin and keep only the local one
+# TODO
+
+# Remove repo
+# TODO
+
+# Add announcement that this script has just finished with timestamp
+# TODO
