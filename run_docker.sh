d4f375bc96b9b2484f0dcde29ff1e5f782316c20
Oleksandr6676 <oleksandr6676@gmail.com>
2021-12-04 20:20:13 +0100

    Minor: Run_docker fixes and improvements Implemented requested fixes and improvements Closes #13

--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Added color variables
-red='\033[0;31m'
-nc='\033[0m'
+# Added color constants
+RED='\033[0;31m'
+NC='\033[0m'
 
 usage() {
   echo "Usage: ./run_docker username password"
@@ -14,74 +14,75 @@ usage() {
 
 # Add validation to given arguments. If the parameters are not set or are empty, show usage message.
 # TODO
-username=oleksandr6676
-password=q532484966q
+username="$1"
+password="$2"
 if [[ -z "$username" || -z "$password" ]]; then
   usage
   exit
 fi
 
 # If you have no account on docker hub, sign up yourself. Then, login yourself to docker
-echo -e "${red}Logging in to Docker hub${nc}"
-docker login --username="$username" --password="$password"
+echo -e "${RED}Logging in to Docker hub${NC}"
+echo -n "$password" | docker login --username="$username" --password-stdin
 
 # Pull Alpine Linux (alpine:3.15.0) docker image
-echo -e "${red}Pulling docker image${nc}"
+echo -e "${RED}Pulling docker image${NC}"
 image_name=alpine:3.15.0
 docker pull "$image_name"
 
 # List docker images
-echo -e "${red}List docker images${nc}"
+echo -e "${RED}List docker images${NC}"
 docker images
 
 # Launch the docker image from previous step with 'ls -la /' command
-# TODO
+echo -e "${RED}Starting container with ls -la command${NC}"
+docker run "$image_name" ls -la
 
 # Start a new container with pulled Alpine Linux image
-echo -e "${red}Starting new container with pulled image${nc}"
+echo -e "${RED}Starting new container with pulled image${NC}"
 container_name=my_container
-docker run -it -d --name "$container_name" "$image_name"
+docker run --name "$container_name" -d "$image_name" sh -c "while true; do echo 'Show me some logs'; sleep 1; done"
 
 # List all running docker containers
-echo -e "${red}List running containers${nc}"
+echo -e "${RED}List running containers${NC}"
 docker ps
 
 # List environment variables of running alpine container and the output store to 'env_vars' file in home directory in the running container
-echo -e "${red}List env variables of running container and save it to the file${nc}"
+echo -e "${RED}List env variables of running container and save it to the file${NC}"
 env_variables=$(docker exec "$container_name" env)
 docker exec "$container_name" sh -c "echo '$env_variables' >> home/env_vars"
 docker exec "$container_name" cat home/env_vars
 
 
 # Copy 'clone_update_repo.sh' script from this repo to home directory in the running container
-echo -e "${red}Copying file from host to container${nc}"
+echo -e "${RED}Copying file from host to container${NC}"
 docker cp clone_update_repo.sh "$container_name":home
 docker exec "$container_name" ls home
 
 # Show logs from the running container
-echo -e "${red}Logs from the running container:${nc}"
+echo -e "${RED}Logs from the running container:${NC}"
 docker logs "$container_name"
 
 # Stop the running container
-echo -e "${red}Stop the container${nc}"
+echo -e "${RED}Stop the container${NC}"
 docker stop -t 1 "$container_name"
 
 # Create your own image named 'custom-alpine:0.0.1' from the stopped container
-echo -e "${red}Creating custom image with tag${nc}"
-default_tag=oleksandr6676/custom-alpine:0.0.1
+echo -e "${RED}Creating custom image with tag${NC}"
+default_tag="$username/custom-alpine:0.0.1"
 docker commit "$container_name" "$default_tag"
 docker images
 
 # Create a new tag '0.0.2' of your new image
-echo -e "${red}Creating new tag for custom image${nc}"
-updated_tag=oleksandr6676/custom-alpine:0.0.2
+echo -e "${RED}Creating new tag for custom image${NC}"
+updated_tag="$username/custom-alpine:0.0.2"
 docker tag "$default_tag" "$updated_tag"
 docker images
 
 # Push your new image to docker hub
-echo -e "${red}Pushing new image to docker hub${nc}"
+echo -e "${RED}Pushing new image to docker hub${NC}"
 docker push "$updated_tag"
 
 # Logout yourself from docker
-echo -e "${red}Logged out${nc}"
+echo -e "${RED}Logged out${NC}"
 docker logout
