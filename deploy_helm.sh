a4f2a46fbdc515ce11dbced178bc8addb07a4bd1
Oleksandr6676 <36409046+Oleksandr6676@users.noreply.github.com>
2022-04-21 08:45:48 +0200

    fix: fix helm deploy script
    implements changes and improvements to helm deploy script
    
    Closes #54;

--- deploy_helm.sh
+++ deploy_helm.sh
@@ -7,42 +7,55 @@ NC='\033[0m'
 # Add announcement that this script has just started with timestamp
 echo -e "${RED}Script started at $(date +%T)${NC}"
 
-# Create dev environment if not exist
-echo -e "${RED}Creating dev environment${NC}"
-kubectl create namespace dev
+while getopts ":e:n:" o; do
+    case "${o}" in
+        e) env_name=$OPTARG;;
+        n) chart_name=$OPTARG;;
+        *) echo "No reasonable options found"
+    esac
+done
 
-# Create staging environment if not exist
-echo -e "${RED}Creating staging environment${NC}"
-kubectl create namespace stag
-
-# Create production environment if not exist
-echo -e "${RED}Creating production environment${NC}"
-kubectl create namespace prod
-
-# Read the argument with selected namespace
-ns="${1:-"dev"}"
-if [ "$ns" = "dev" ] || [ "$ns" = "stag" ] || [ "$ns" = "prod" ]; then
-    echo -e "${RED}Environment selected for helm chart deployment:${NC}"
-    echo "$ns"
+# Create storage-class if not exists
+get_sc=$(kubectl get sc "happy-bank-local-storage" --no-headers 2>/dev/null)
+if [ -n "${get_sc}" ]; then
+    echo -e "${RED}Storage class exists:${NC}"
+    kubectl get sc
 else
-    echo -e "${RED}Environment with given name doesn't exist${NC}"
-    echo -e "${RED}Script ended at $(date +%T)${NC}"
-    exit
+    echo "Creating storage class"
+    kubectl apply -f kubernetes/storage-class.yml
+fi
+
+# Create namespace
+get_ns=$(kubectl get ns "$env_name" --no-headers 2>/dev/null)
+if [ -n "${get_ns}" ]; then
+    echo -e "${RED}Environment selected for helm chart deployment:${NC}"
+    echo "$get_ns"
+else
+    echo "Namespace $env_name not found"
+    echo "Creating namespace $env_name"
+    kubectl create ns "$env_name"
 fi
 
 # Generate secret token and pass it to values.yaml
 echo -e "${RED}Generating secret token${NC}"
 token="$(date +%T | sha1sum | awk '{print $1}')"
-echo "$token"
 sed -i "s/<CORE_TOKEN>/$token/g" ./helm/values.yaml
 
 # Deploy helm chart to selected namespace
-echo -e "${RED}Deploying helm chart${NC}"
-chart_name="${2:-"happy-bank"}"
-helm install "$chart_name" --namespace "$ns" ./helm
+echo -e "${RED}Deploying Helm Chart${NC}"
+get_helm_charts=$(helm list --namespace "$env_name" | grep "$chart_name")
+if [ -n "${get_helm_charts}" ]; then
+    echo "Helm chart with given name already exists:"
+    echo "$get_helm_charts"
+    echo -e "${RED}Script ended at $(date +%T)${NC}"
+    exit
+else
+    helm install "$chart_name" --namespace "$env_name" ./helm
+fi
 
 # List helm charts in selected namespace
 echo -e "${RED}Deployed helm charts in selected namespace${NC}"
-helm list --namespace "$ns"
+helm list --namespace "$env_name"
 
-
+# Add announcement that this script has ended with timestamp
+echo -e "${RED}Script ended at $(date +%T)${NC}"
